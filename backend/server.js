4f543350f855d702238e03cc7e3f8cbb4d98c22f
Brito-Reji <britoreji03@gmail.com>
2025-04-01 22:14:43 +0530

    feat:delete item

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@ import Product from "./models/product.model.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ data: products, sucess: true });
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.post("/api/products", async (req, res) => {
   const product = req.body;
   if (!(product.name || product.price || product.image)) {
@@ -22,6 +30,22 @@ app.post("/api/products", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "delted",
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "product not found",
+      success: false,
+    });
   }
 });
 
