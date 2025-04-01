136c4ceae0765856197777038b3072f6c3ac6cbf
Brito-Reji <britoreji03@gmail.com>
2025-04-01 15:52:24 +0530

    feat:dtabase connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
 app.listen(5000, () => {
+  connectDB();
   console.log("server running on http://localhost:5000");
 });
