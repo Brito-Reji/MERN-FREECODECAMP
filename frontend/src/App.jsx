0f90cdc5107b258b0ac05bc2e87c56a044d2f202
Brito-Reji <britoreji03@gmail.com>
2025-04-02 16:02:40 +0530

    feat:navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import { Button } from "@chakra-ui/react";
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import { Box } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box minH={"100vh"}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "@chakra-ui/react/preset";
 
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider value={system}>
+      <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
