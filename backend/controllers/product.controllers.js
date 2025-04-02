89e9009a5f1dd2cdd468d8584bea162bd38e2be7
Brito-Reji <britoreji03@gmail.com>
2025-04-02 10:42:42 +0530

    reafactor: product controller created

---
+++ backend/controllers/product.controllers.js
@@ -0,0 +1,65 @@
+import Product from "../models/product.model.js";
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ data: products, sucess: true });
+  } catch (error) {
+    console.log("there id an error while feching products", error);
+  }
+};
+
+const createProduct = async (req, res) => {
+  const product = req.body;
+  if (!(product.name || product.price || product.image)) {
+    res.status(400).json({
+      success: false,
+      message: "please provide all fields",
+    });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ message: "no id found", sucess: false });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    4;
+    res.status(200).json({ success: true, message: updatedProduct });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "server error" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "delted",
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "product not found",
+      success: false,
+    });
+  }
+};
+
+export { createProduct, deleteProduct, updateProduct, getProducts };
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+import ProductRoutes from "./routes/product.router.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use("/api/products", ProductRoutes);
 app.listen(5000, () => {
   connectDB();
   console.log("server running on http://localhost:5000");
